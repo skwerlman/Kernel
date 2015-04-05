47c87a63c928a8bc452fc14d94b93fa2217444b4
Matheus de Alcantara <me@matheus.ninja>
2015-04-04 22:36:01 -0300

    Squish! this time, modw, sysw and tasks.list to the main executable.

--- kernel/main.lua
+++ kernel/main.lua
@@ -51,15 +51,78 @@ end
 
 logf('TARDIX kernel version 2015-MARCH')
 
-local ok, err =  _newThread(loadfile(fs.combine(_G.params.root, '/workers/sysw.lua'))):start()
+local function listAll(_path, _files)
+  local path = _path or ""
+  local files = _files or {}
+  if #path > 1 then table.insert(files, path) end
+  for _, file in ipairs(fs.list(path)) do
+    local path = fs.combine(path, file)
+    if fs.isDir(path) then
+      listAll(path, files)
+    else
+      table.insert(files, path)
+    end
+  end
+  return files
+end
+
+--logf('module worker starting')
+local list = (listAll( fs.combine(_G.params.root, '/modules')))
+
+for k, v in pairs(list) do
+  if not fs.isDir(v) then
+    dofile(v)
+  end
+end
+
+modules.loadAllModules()
+local function _newThread(f)
+  local thread = {
+    ['coro'] = coroutine.create(f)
+  }
+
+  function thread:start(...)
+    return coroutine.resume(self.coro, ...)
+  end
+
+  function thread:stop()
+    self.coro = nil -- should force lua to garbace collect
+  end
+
+  function thread:args(...)
+    return coroutine.resume(self.coro, ...)
+  end
+
+  return thread
+end
 
 
-local _elapsed = math.floor(os.clock() - _starttime)
+-- pass control to userland
+-- hardcoded
 
+function _run(init)
+  local __ok, err =  loadfile(init)
+  if not __ok then
+    logf('[critical] failed to load init \'%s\'. \n\t error : %s', init, err)
+  end
 
-if not ok then
-  logf('[critical] error on system worker. \n\t error : %s', err)
-  while true do
-    sleep(1)
+  local ok, err = _newThread(__ok):start()
+end
+
+local inits = {
+  '/init',
+  '/sbin/init',
+  '/bin/init',
+  '/lib/init',
+  '/usr/init',
+  '/usr/sbin/init',
+  '/usr/bin/init',
+  '/usr/lib/init',
+}
+
+for i = 1, #inits do
+  if fs.exists(inits[i]) then
+    _run(inits[i])
+    break
   end
 end
