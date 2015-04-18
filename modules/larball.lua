0a03d5a114b2eb30aff3cfd97def5d9b21c3b19d
DemHydraz <matheus.de.alcantara@gmail.com>
2015-04-18 18:59:09 -0300

    Fixed identation!
    Thank you, Atom.

--- modules/larball.lua
+++ modules/larball.lua
@@ -2,32 +2,32 @@ local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 
 -- encoding
 local function enc(data)
-    return ((data:gsub('.', function(x)
-        local r,b='',x:byte()
-        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
-        return r;
-    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
-        if (#x < 6) then return '' end
-        local c=0
-        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
-        return b:sub(c+1,c+1)
-    end)..({ '', '==', '=' })[#data%3+1])
+  return ((data:gsub('.', function(x)
+    local r,b='',x:byte()
+    for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+    return r;
+  end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+    if (#x < 6) then return '' end
+    local c=0
+    for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+    return b:sub(c+1,c+1)
+  end)..({ '', '==', '=' })[#data%3+1])
 end
 
 -- decoding
 local function dec(data)
-    data = string.gsub(data, '[^'..b..'=]', '')
-    return (data:gsub('.', function(x)
-        if (x == '=') then return '' end
-        local r,f='',(b:find(x)-1)
-        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
-        return r;
-    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-        if (#x ~= 8) then return '' end
-        local c=0
-        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
-        return string.char(c)
-    end))
+  data = string.gsub(data, '[^'..b..'=]', '')
+  return (data:gsub('.', function(x)
+    if (x == '=') then return '' end
+    local r,f='',(b:find(x)-1)
+    for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+    return r;
+  end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+    if (#x ~= 8) then return '' end
+    local c=0
+    for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+    return string.char(c)
+  end))
 end
 
 local function _do_tabling(file)
@@ -67,95 +67,95 @@ function _G.tt(str, size)
     a[i] = str:sub(1,size)
     i = i + 1
     str = str:sub(size+1)
-  until str == ''
-  return a
-end
-
-local function _write_larball(file, data)
-  local file_h = fs.open(file, 'w')
-  local data = enc(textutils.serialize(data))
-  local count = 0
-
-  for k, v in pairs(tt(data, 64)) do
-    file_h.writeLine(v)
-    count = count + 1
-    if count == 16 then
-      file_h.writeLine('')
-      count = 0
-    end
-  end
-  file_h.close()
-end
-
-local function _do_unlarring(root, data)
-  for i = 1, #data do
-    local file = fs.open(fs.combine(root, data[i].meta.path), 'w')
-    file.writeLine(data[i].data)
-    file.close()
+    until str == ''
+    return a
   end
 
-end
+  local function _write_larball(file, data)
+    local file_h = fs.open(file, 'w')
+    local data = enc(textutils.serialize(data))
+    local count = 0
 
-local function _do_unlarballing(rootdir, file)
-  local data = fs.open(file, 'r')
-  local tab = textutils.unserialize(dec(data.readAll()))
-  data.close()
-
-
-  _do_unlarring(rootdir, tab)
-end
-
-local _lar = modules.module 'larballs' {
-  text = {
-    load = function()
-      _G.larball = {
-        ['lar'] = function(file, dir)
-          _write_larball(file, _do_larring(dir))
-
-        end,
-        ['unlar'] = _do_unlarballing,
-        ['unlarToRoot'] = function(file)
-          _do_unlarballing('/', file)
-        end,
-        ['untab'] = _do_unlarring,
-        ['remlar'] = function(root, file)
-          local data = fs.open(file, 'r')
-          local tab = textutils.unserialize(dec(data.readAll()))
-          data.close()
-
-          for k, v in pairs(tab) do
-            fs.delete(fs.combine(root, v.meta.path))
-          end
-        end,
-        ['remtab'] = function(root, tab)
-          for k, v in pairs(tab) do
-            fs.delete(fs.combine(root, v.meta.path))
-          end
-        end
-      }
-    end,
-    unload = function()
-      _G.larball = nil
+    for k, v in pairs(tt(data, 64)) do
+      file_h.writeLine(v)
+      count = count + 1
+      if count == 16 then
+        file_h.writeLine('')
+        count = 0
+      end
     end
-  }
-}
+    file_h.close()
+  end
 
-local _base64 = modules.module 'base64' {
-  text = {
-    load = function()
-      _G.base64 = {
-        ['encode'] = enc,
-        ['decode'] = dec,
-      }
-    end,
-    unload = function()
-      _G.base64 = nil
+  local function _do_unlarring(root, data)
+    for i = 1, #data do
+      local file = fs.open(fs.combine(root, data[i].meta.path), 'w')
+      file.writeLine(data[i].data)
+      file.close()
     end
+
+  end
+
+  local function _do_unlarballing(rootdir, file)
+    local data = fs.open(file, 'r')
+    local tab = textutils.unserialize(dec(data.readAll()))
+    data.close()
+
+
+    _do_unlarring(rootdir, tab)
+  end
+
+  local _lar = modules.module 'larballs' {
+    text = {
+      load = function()
+        _G.larball = {
+          ['lar'] = function(file, dir)
+            _write_larball(file, _do_larring(dir))
+
+          end,
+          ['unlar'] = _do_unlarballing,
+          ['unlarToRoot'] = function(file)
+            _do_unlarballing('/', file)
+          end,
+          ['untab'] = _do_unlarring,
+          ['remlar'] = function(root, file)
+            local data = fs.open(file, 'r')
+            local tab = textutils.unserialize(dec(data.readAll()))
+            data.close()
+
+            for k, v in pairs(tab) do
+              fs.delete(fs.combine(root, v.meta.path))
+            end
+          end,
+          ['remtab'] = function(root, tab)
+            for k, v in pairs(tab) do
+              fs.delete(fs.combine(root, v.meta.path))
+            end
+          end
+        }
+      end,
+      unload = function()
+        _G.larball = nil
+      end
+    }
   }
-}
+
+  local _base64 = modules.module 'base64' {
+    text = {
+      load = function()
+        _G.base64 = {
+          ['encode'] = enc,
+          ['decode'] = dec,
+        }
+      end,
+      unload = function()
+        _G.base64 = nil
+      end
+    }
+  }
 
 
-local function _se_append_header(file)
+  local function _se_append_header(file)
   file.writeLine('local b=\'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/\'')
   file.writeLine("local function dec(data) data = string.gsub(data, '[^'..b..'=]', '') return (data:gsub('.', function(x) if (x == '=') then return '' end local r,f='',(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end return r; end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) if (#x ~= 8) then return '' end local c=0 for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end return string.char(c) end)) end")
 
