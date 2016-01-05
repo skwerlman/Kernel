d29535e5b1181485b3d721cad773d12412144fe6
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2016-01-05 10:14:40 -0200

    unexpand tests/1-verify-test-framework.lua

--- tests/1-verify-test-framework.lua
+++ tests/1-verify-test-framework.lua
@@ -1,25 +1,25 @@
 -- Test testing framework
 
 test.manifest = {
-    ["desc"] = "verify the testing framework works.",
-    ["important"] = true,
-    ["onFail"] = "Warning, the testing framework has failed. Test may not work.",
-    ["shouldFail"] = false,
+		["desc"] = "verify the testing framework works.",
+		["important"] = true,
+		["onFail"] = "Warning, the testing framework has failed. Test may not work.",
+		["shouldFail"] = false,
 }
 
 test.run = function(this)
-  if this == nil then
-    error("test not providing this object, got nil");
-  end
+	if this == nil then
+		error("test not providing this object, got nil");
+	end
 
-  if this.manifest == nil then
-    error("test manifest is empty.");
-  end
+	if this.manifest == nil then
+		error("test manifest is empty.");
+	end
 
-  if error == nil then
-    print("critical: error is not hijacked.")
-    os.exit(1);
-  end
+	if error == nil then
+		print("critical: error is not hijacked.")
+		os.exit(1);
+	end
 end
 
 -- return the test object
